c808d989ed4b4bfd681bcf6c64837819737dd985
Andrew Riebe <anubisascends@gmail.com>
2026-07-02 07:58:45 -0400

    feat: add location detail page with set summary and card list

---
+++ OmniCard.Web/Pages/Location.cshtml.cs
@@ -0,0 +1,74 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using OmniCard.Data;
+using OmniCard.Models;
+
+namespace OmniCard.Web.Pages;
+
+public class LocationModel : PageModel
+{
+    private readonly IDbContextFactory<CollectionDbContext> _dbFactory;
+
+    public LocationModel(IDbContextFactory<CollectionDbContext> dbFactory)
+    {
+        _dbFactory = dbFactory;
+    }
+
+    public StorageContainer Container { get; set; } = null!;
+    public int CardCount { get; set; }
+    public List<SetSummary> Sets { get; set; } = [];
+    public List<CollectionCard> Cards { get; set; } = [];
+
+    public IActionResult OnGet(int id)
+    {
+        using var db = _dbFactory.CreateDbContext();
+
+        var container = db.StorageContainers
+            .AsNoTracking()
+            .FirstOrDefault(c => c.Id == id);
+
+        if (container is null)
+            return NotFound();
+
+        Container = container;
+
+        Cards = db.Cards
+            .AsNoTracking()
+            .Where(c => c.ContainerId == id)
+            .OrderBy(c => c.Name)
+            .ToList();
+
+        CardCount = Cards.Count;
+
+        Sets = Cards
+            .GroupBy(c => new { c.SetCode, c.SetName })
+            .Select(g => new SetSummary
+            {
+                SetCode = g.Key.SetCode,
+                SetName = g.Key.SetName,
+                Count = g.Count(),
+            })
+            .OrderBy(s => s.SetName)
+            .ToList();
+
+        return Page();
+    }
+
+    public string TypeDisplay => Container.ContainerType switch
+    {
+        ContainerType.Bulk => "Bulk",
+        ContainerType.Binder => "Binder",
+        ContainerType.Box => "Box",
+        ContainerType.DeckBox => "Deck Box",
+        ContainerType.DisplayCase => "Display Case",
+        _ => Container.ContainerType.ToString(),
+    };
+
+    public record SetSummary
+    {
+        public string SetCode { get; init; } = "";
+        public string SetName { get; init; } = "";
+        public int Count { get; init; }
+    }
+}
