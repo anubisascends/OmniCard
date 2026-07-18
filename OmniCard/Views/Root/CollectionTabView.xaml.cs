2f9b97dfe6a0c03a9dd8707231ac80ad2dd43e1c
Andrew Riebe <anubisascends@gmail.com>
2026-07-17 23:39:58 -0400

    feat: remove column chooser now that card list uses tiles

--- OmniCard/Views/Root/CollectionTabView.xaml.cs
+++ OmniCard/Views/Root/CollectionTabView.xaml.cs
@@ -1,5 +1,3 @@
-using System.ComponentModel;
-using System.Windows;
 using System.Windows.Controls;
 using OmniCard.Models;
 
@@ -8,8 +6,6 @@ namespace OmniCard.Views.Root;
 public partial class CollectionTabView : UserControl
 {
     public RootViewModel? ViewModel { get; set; }
-    private CollectionViewModel? _wiredVm;
-    private PropertyChangedEventHandler? _vmHandler;
 
     public CollectionTabView()
     {
@@ -18,41 +14,7 @@ public partial class CollectionTabView : UserControl
 
     public void WireUp(CollectionViewModel vm)
     {
-        if (_wiredVm is not null && _vmHandler is not null)
-            _wiredVm.PropertyChanged -= _vmHandler;
-
-        _wiredVm = vm;
         CardList.WireUp(vm);
-        BuildColumnChooser(vm);
-        _vmHandler = (_, e) =>
-        {
-            if (e.PropertyName == nameof(CollectionViewModel.ColumnVisibility))
-                BuildColumnChooser(vm);
-        };
-        vm.PropertyChanged += _vmHandler;
-    }
-
-    private void BuildColumnChooser(CollectionViewModel vm)
-    {
-        ColumnChooserList.Items.Clear();
-        foreach (var kvp in vm.ColumnVisibility)
-        {
-            var cb = new CheckBox
-            {
-                Content = kvp.Key,
-                IsChecked = kvp.Value,
-                Margin = new Thickness(4, 2, 4, 2)
-            };
-            var column = kvp.Key;
-            cb.Checked += (_, _) => vm.ToggleColumnVisibility(column);
-            cb.Unchecked += (_, _) => vm.ToggleColumnVisibility(column);
-            ColumnChooserList.Items.Add(cb);
-        }
-    }
-
-    private void ColumnChooserLink_Click(object sender, RoutedEventArgs e)
-    {
-        ColumnChooserPopup.IsOpen = !ColumnChooserPopup.IsOpen;
     }
 
     public void FocusSearchBox()
